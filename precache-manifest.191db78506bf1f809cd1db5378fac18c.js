36a97dd3fffa6a0538b2d91526b350e188d454e3
shelooks16 <blauyedz@zoho.eu>
2020-10-06 11:16:43 +0300

    Updates

---
+++ precache-manifest.191db78506bf1f809cd1db5378fac18c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a5c7fb935fa5befe0d7bf86faa97b3",
+    "url": "/assignment-cart/index.html"
+  },
+  {
+    "revision": "682a65a2c247aa2442c3",
+    "url": "/assignment-cart/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c1625fcb06c2952a7456",
+    "url": "/assignment-cart/static/css/main.1a5150fb.chunk.css"
+  },
+  {
+    "revision": "682a65a2c247aa2442c3",
+    "url": "/assignment-cart/static/js/2.b317af28.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/assignment-cart/static/js/2.b317af28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1625fcb06c2952a7456",
+    "url": "/assignment-cart/static/js/main.cca99580.chunk.js"
+  },
+  {
+    "revision": "4c700afb5751f1cdeaa9",
+    "url": "/assignment-cart/static/js/runtime-main.e562474c.js"
+  },
+  {
+    "revision": "20de7eec1e932d4aa766854aef77ecc0",
+    "url": "/assignment-cart/static/media/cart.20de7eec.svg"
+  }
+]);
