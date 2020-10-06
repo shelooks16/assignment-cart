4888e9131c49f69935db2c66d6ff7caf92fd4faa
shelooks16 <blauyedz@zoho.eu>
2020-10-06 05:19:48 +0300

    create links to category pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import categories from '../db/categories.json';
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <div>
+        {categories.map(c => (
+          <div key={c.id}>
+            <Link to={`/category/${c.id}`}>{c.name}</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
